e50369773f6e33a625bafde7e0901989118e1a50
mohamad-amin <mohamadamin@aut.ac.ir>
2016-12-26 19:46:30 +0330

    Improved start command with keyboard and selections

--- src/commands/_StartCommand.php
+++ src/commands/_StartCommand.php
@@ -2,6 +2,8 @@
 namespace Longman\TelegramBot\Commands\UserCommands;
 
 use Longman\TelegramBot\Commands\UserCommand;
+use Longman\TelegramBot\Conversation;
+use Longman\TelegramBot\Entities\Keyboard;
 use Longman\TelegramBot\Request;
 
 /**
@@ -14,15 +16,49 @@ class _StartCommand extends UserCommand
     protected $description = 'شروع کار با ربات';
     protected $usage = '/start';
 
+    /** @var  \Telegram */
+    protected $telegram;
+    protected $conversation;
+
     public function execute()
     {
 
         $message = $this->getMessage();
+        $chat = $message->getChat();
+        $user = $message->getFrom();
+        $chat_id = $chat->getId();
+        $user_id = $user->getId();
+        $text = $message->getText();
+
         $data = [];
-        $data['chat_id'] = $message->getChat()->getId();
+        $send = false;
+        $this->conversation = new Conversation($user_id, $chat_id, $this->getName());
 
-        $data['text'] = 'تست تست';
+        if ($text == 'ارسال جزوه') {
+            $this->conversation->stop();
+            $this->telegram->executeCommand("submit");
+            $send = true;
+        } else if ($text == 'درباره ربات') {
+            $this->conversation->stop();
+            $this->telegram->executeCommand("about");
+            $send = true;
+        } else {
+            $keyboard = new Keyboard(
+                ['درباره ربات', 'ارسال جزوه']
+            );
+            $keyboard->setResizeKeyboard(true);
+            $data = [
+                'chat_id' => $chat_id,
+                'text' => 'یکی از گزینه‌ها رو انتخاب کنید.',
+                'keyboard' => $keyboard
+            ];
+        }
 
-        return Request::sendMessage($data);
+        if ($send) {
+            return Request::sendMessage($data);
+        } else {
+            return true;
+        }
+
     }
 }
